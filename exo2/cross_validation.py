416af422f74bb63b95d5bd487cee40269fbbe360
gonzalez_david <david.gonzalez0005@gmail.com>
2018-05-25 21:34:51 +0200

    Implement plot and generalise to multiple models

--- exo2/cross_validation.py
+++ exo2/cross_validation.py
@@ -1,7 +1,7 @@
 """
 Detemine the best model by cross-validation: http://scikit-learn.org/stable/model_selection.html
 
-Model to test::
+Model to test:
   - KNeighborsClassifier:   http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
   - DecisionTreeClassifier: http://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
   - MLPClassifier:          http://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html
@@ -13,6 +13,7 @@ Author: Claudio Sousa, David Gonzalez
 
 from pprint import pprint
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
@@ -24,60 +25,76 @@ import numpy as np
 # Constants
 CV_SEGMENT = 5
 CV_REPEAT = 10
-CV_PARAM_NUM = 10
 CV_MLP_MAX_LAYER = 2
 
 # Data to work with
-data = datasets.load_wine()
-#data = datasets.load_breast_cancer()
-X = data.data
-Y = data.target
+#data = datasets.load_wine()
+data = datasets.load_breast_cancer()
 
-# Generate models to train
-models = [KNeighborsClassifier]
-model_values = [
-    list(range(1, 11))
+# Data structure to hold models and generated info
+models = [
+    {
+        "model_constructor": KNeighborsClassifier,
+        "model_variations": [
+            {
+                "model": KNeighborsClassifier(n_neighbors=i),
+                "tests": [],
+                "avg": 0,
+                "val": i
+            } for i in range(1, 11)] ,
+        "best": None
+    },
+    {
+        "model_constructor": DecisionTreeClassifier,
+        "model_variations": [
+            {
+                "model": DecisionTreeClassifier(min_samples_leaf=i),
+                "tests": [],
+                "avg": 0,
+                "val": i
+            } for i in range(1, 11)] ,
+        "best": None
+    }
 ]
-model_variations = [[] for _ in range(len(models))]
-for v in model_values[0]:
-    model_variations[0].append(models[0](n_neighbors=v))
+best_model = None
 
-# Cross-validate each model variation
-# Array dimension: 1: model, 2: model variation, 3: subdataset index
-scores = [
-    [
-        [
-            [] for _ in range(CV_SEGMENT * CV_REPEAT)
-        ] for _ in range(CV_PARAM_NUM)
-    ] for _ in range(len(models))
-]
-
+# Fill the tests result for each model variations,
+# compute their average and get the best model variation by model type
 rkf = RepeatedKFold(n_splits=CV_SEGMENT, n_repeats=CV_REPEAT)
+for m in models:
+    for mvar in m["model_variations"]:
+        for train_index, test_index in rkf.split(data.data):
+            mvar["model"].fit(data.data[train_index], data.target[train_index])
+            mvar["tests"].append(mvar["model"].score(data.data[test_index], data.target[test_index]))
 
-for m_i, m in enumerate(model_variations):
-    for mvar_i, model in enumerate(m):
-        for train_i, indexes in enumerate(rkf.split(X)):
-            train_index, test_index = indexes
+        mvar["avg"] = float(np.mean(mvar["tests"]))
 
-            model.fit(X[train_index], Y[train_index])
-            scores[m_i][mvar_i][train_i] = model.score(X[test_index], Y[test_index])
+    m["best"] = m["model_variations"][np.array([mvar["avg"] for mvar in m["model_variations"]]).argmax()]
 
-pprint(scores)
+# Get the best model from all best model variations
+best_model = models[np.array([m["best"]["avg"] for m in models]).argmax()]
 
-# Compute score average
-# Array dimension: 1: model, 2: model variation
-score_avgs = [
-    [
-        [] for _ in range(CV_PARAM_NUM)
-    ] for _ in range(len(models))
-]
-
-for m_i, m in enumerate(scores):
-    for mvar_i, mvar in enumerate(m):
-       score_avgs[m_i][mvar_i] = float(np.mean(mvar))
-
-pprint(score_avgs)
-
-# Compute best value
-for m_i, m in enumerate(score_avgs):
-    print("Best K:", model_values[m_i][np.array(m).argmax()])
+# Plot all
+ax = plt.figure().add_subplot(111, projection='3d')
+plt.title("Models performance by variations, their average, their best average and best")
+for m_i, m in enumerate(models):
+    ax.scatter(
+        [[mvar_i] * len(mvar["tests"]) for mvar_i, mvar in enumerate(m["model_variations"])],
+        [[m_i] * len(mvar["tests"]) for mvar in m["model_variations"]],
+        [mvar["tests"] for mvar in m["model_variations"]],
+        s=40
+    )
+for m_i, m in enumerate(models): # Remake loop to allow plt.legend() to get the right color for each model
+    ax.scatter(
+        [mvar_i for mvar_i, mvar in enumerate(m["model_variations"])],
+        [m_i for mvar in m["model_variations"]],
+        [mvar["avg"] for mvar in m["model_variations"]],
+        c=["black" if m["best"] != mvar else "green" if m != best_model else "red" for mvar in m["model_variations"]],
+        s=200
+    )
+plt.legend([m["model_constructor"].__name__ for m in models], loc='lower right')
+plt.grid(True)
+ax.set_xlabel("Model variations")
+ax.set_ylabel("Model")
+ax.set_zlabel("Performance")
+plt.show()
