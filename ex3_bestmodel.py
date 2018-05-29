c9e06078007b57b4c2455d87a202c61a0afe4a89
gonzalez_david <david.gonzalez0005@gmail.com>
2018-05-29 15:18:06 +0200

    Implement 3rd part

---
+++ ex3_bestmodel.py
@@ -0,0 +1,36 @@
+"""
+Launch a cross-validation on three models to see which is best on custom data from:
+    https://archive.ics.uci.edu/ml/datasets.html
+
+Chosen data: https://archive.ics.uci.edu/ml/datasets/Leaf
+Tested models: KNeighborsClassifier, DecisionTreeClassifier and MLPClassifier.
+
+Author: Claudio Sousa, David Gonzalez
+"""
+
+from sklearn import datasets
+from cross_validation import cross_validate, plot_validation, normalise_data
+from models import instanciate_svc_model, instanciate_randomforest_model, instanciate_decisiontree_model
+import numpy as np
+from sklearn.datasets.base import Bunch
+import pandas as pd
+
+from models import instanciate_kneighbors_model, instanciate_decisiontree_model, instanciate_mlp_model
+from pprint import pprint
+
+csv = pd.read_csv("data/leaf.csv")
+data = Bunch(
+    data=np.array([list(d[1:]) for d in csv.values]),
+    target=np.array([d[0] for d in csv.values])
+)
+#pprint(data)
+data.data = normalise_data(data.data)
+
+models = [
+    instanciate_svc_model(),
+    instanciate_randomforest_model(1, 11),
+    instanciate_decisiontree_model(1, 11)
+]
+
+best_model = cross_validate(data, models, 5, 10)
+plot_validation(models, best_model)
--- models.py
+++ models.py
@@ -7,6 +7,10 @@ Author: Claudio Sousa, David Gonzalez
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import ConstantKernel, WhiteKernel, RBF, Matern, RationalQuadratic, ExpSineSquared, DotProduct
 
 def instanciate_kneighbors_model(n_neighbors_min, n_neighbors_max, n_neighbors_step=1):
     """
@@ -52,7 +56,7 @@ def instanciate_decisiontree_model(min_samples_leaf_min, min_samples_leaf_max, m
 
 def instanciate_mlp_model():
     """
-    Instanciate MLPClassifier n times.
+    Instanciate MLPClassifier with all different solvers and activator functions.
     http://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html
     """
     return {
@@ -67,5 +71,60 @@ def instanciate_mlp_model():
             for activator in ['identity', 'logistic', 'tanh', 'relu']
         ],
         "x_label": "Solver & activator",
+        "rotate_x_labels": 90
+    }
+
+def instanciate_svc_model():
+    """
+    Instanciate SVC with different kernel.
+    http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
+    """
+    return {
+        "model_constructor": SVC,
+        "model_variations": [
+            {
+                "model": SVC(kernel=kernel),
+                "label": kernel
+            } for kernel in ['linear', 'poly', 'rbf', 'sigmoid']
+        ],
+        "x_label": "Kernel",
+        "rotate_x_labels": 90
+    }
+
+def instanciate_randomforest_model(min_samples_leaf_min, min_samples_leaf_max, min_samples_leaf_step=1):
+    """
+    Instanciate RandomForestClassifier by varying the 'min_samples_leaf' parameter.
+    http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html
+
+    Keyword arguments:
+    min_samples_leaf_min -- parameter variation start
+    min_samples_leaf_max -- parameter variation end
+    min_samples_leaf_step -- parameter variation step
+    """
+    return {
+        "model_constructor": RandomForestClassifier,
+        "model_variations": [
+            {
+                "model": RandomForestClassifier(min_samples_leaf=i),
+                "label": i
+            } for i in range(min_samples_leaf_min, min_samples_leaf_max, min_samples_leaf_step)
+        ],
+        "x_label": "Min samples leaf"
+    }
+
+def instanciate_gaussianprocess_model():
+    """
+    Instanciate GaussianProcessClassifier with different kernels.
+    http://scikit-learn.org/stable/modules/generated/sklearn.gaussian_process.GaussianProcessClassifier.html
+    """
+    return {
+        "model_constructor": GaussianProcessClassifier,
+        "model_variations": [
+            {
+                "model": GaussianProcessClassifier(kernel=kernel(), random_state=0, n_jobs=-1),
+                "label": kernel.__name__
+            } for kernel in [ConstantKernel, WhiteKernel, RBF, Matern, RationalQuadratic, ExpSineSquared, DotProduct]
+        ],
+        "x_label": "Kernel",
         "rotate_x_labels": 90
     }
